165b9b1c5555d7c4a0a20e8ab30c1743d7cfc4a2
HHSOLL <KHSOL3141@gmail.com>
2026-03-27 23:39:16 +0900

    fix: migrate iOS app for UIScene debug launches

--- apps/mobile_app/ios/Runner/AppDelegate.swift
+++ apps/mobile_app/ios/Runner/AppDelegate.swift
@@ -3,7 +3,7 @@ import GoogleMaps
 import UIKit
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
   private func hasGoogleMapsKey() -> Bool {
     guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "GOOGLE_MAPS_API_KEY") as? String
     else {
@@ -17,28 +17,30 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     if let apiKey = Bundle.main.object(forInfoDictionaryKey: "GOOGLE_MAPS_API_KEY") as? String,
-      hasGoogleMapsKey() {
+      hasGoogleMapsKey()
+    {
       GMSServices.provideAPIKey(apiKey)
     } else {
       NSLog("record: GOOGLE_MAPS_API_KEY is missing. Google Maps tiles will not load.")
     }
-    GeneratedPluginRegistrant.register(with: self)
-
-    if let controller = window?.rootViewController as? FlutterViewController {
-      let channel = FlutterMethodChannel(
-        name: "travel_atlas/runtime_capabilities",
-        binaryMessenger: controller.binaryMessenger
-      )
-      channel.setMethodCallHandler { [weak self] call, result in
-        switch call.method {
-        case "hasGoogleMapsKey":
-          result(self?.hasGoogleMapsKey() ?? false)
-        default:
-          result(FlutterMethodNotImplemented)
-        }
-      }
-    }
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+
+    let channel = FlutterMethodChannel(
+      name: "travel_atlas/runtime_capabilities",
+      binaryMessenger: engineBridge.applicationRegistrar.messenger()
+    )
+    channel.setMethodCallHandler { [weak self] call, result in
+      switch call.method {
+      case "hasGoogleMapsKey":
+        result(self?.hasGoogleMapsKey() ?? false)
+      default:
+        result(FlutterMethodNotImplemented)
+      }
+    }
   }
 }
